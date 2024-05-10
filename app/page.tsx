2b5bd961ddc19a4dd7f01efc28797dae731b6d1b
Georgiana Barefield <168513490+dsFbhKj@users.noreply.github.com>
2024-05-10 00:56:19 -0700

    made landing page

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,20 @@ export default function Home() {
         </Link>
 
         <Link
+          href="/landing"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="flex justify-center mb-3 text-2xl font-semibold">
+            Landing{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </Link>
+
+        <Link
           href="/login"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
